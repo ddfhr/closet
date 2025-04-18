145fea121b382805ef2079cdd99864f78ae9647e
RYJ <3529742497@qq.com>
2025-04-18 20:46:51 +0800

    初始代码

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -51,10 +51,15 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, IN1_Pin|IN2_Pin|IN3_Pin|IN4_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PAPin PAPin PAPin PAPin */
-  GPIO_InitStruct.Pin = IN1_Pin|IN2_Pin|IN3_Pin|IN4_Pin;
+  /*Configure GPIO pins : PA5 PAPin PAPin PAPin
+                           PAPin */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|IN1_Pin|IN2_Pin|IN3_Pin
+                          |IN4_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
